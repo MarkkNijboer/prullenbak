285afafd9f86bdf845c24eda6e6456b89ffcfa30
Mark Nijboer <mark@HackBook-Pro.local>
2016-01-21 16:32:46 -0800

    GPIO programming

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load("offer_x.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+import RPi.GPIO as GPIO
+GPIO.setup(8, GPIO.OUT)
+while 1:
+    GPIO.output(8, False)
+    sleep(1)
+    GPIO.output(8, True)
+    sleep(1)
