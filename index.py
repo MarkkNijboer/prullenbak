fd28ab080f7f1a81878f938f4a0e8c51d72b64a9
Mark Nijboer <mark@Marks-MacBook-Pro.local>
2017-02-20 13:39:53 +0100

    - Removed LED support

--- index.py
+++ index.py
@@ -20,8 +20,6 @@ import time
 
 # Board IO Pin for sensor
 sensorPin = 10
-
-lightPin = 12
 # Last time called
 called = None
 
@@ -32,9 +30,6 @@ def playSound(channel):
 
   # Check whether last time called was at least a second ago
   if called is None or called <= time.time() - 1:
-
-    GPIO.output(lightPin, 1)
-
     # Get absolute path to 'sounds' folder
     directory = os.path.dirname(os.path.abspath(__file__))+"/sounds/"
     # Get random file from 'sounds' folder
@@ -56,8 +51,6 @@ def playSound(channel):
     # Save current time
     called = time.time()
 
-    GPIO.output(lightPin, 0)
-
 # Catch Keyboardinterrupt to cleanup GPIO channels
 def signal_handler(signal, frame):
   GPIO.cleanup()
@@ -71,8 +64,6 @@ signal.signal(signal.SIGINT, signal_handler)
 GPIO.setmode(GPIO.BOARD)
 # Setup sensor pin and set rest mode to HIGH output.
 GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-GPIO.setup(lightPin, GPIO.OUT)
 # Register interrupt for a rising signal: LOW -> HIGH
 GPIO.add_event_detect(sensorPin, GPIO.RISING, callback=playSound)
 
